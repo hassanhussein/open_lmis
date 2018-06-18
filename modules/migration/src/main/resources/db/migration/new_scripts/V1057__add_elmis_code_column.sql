dbcc4ca04d4ecf984f2df30397601e36d0b0d87e
hassan <hhassan.developer@gmail.com>
2018-06-18 11:57:30 +0300

    Add dashboard changes

--- modules/migration/src/main/resources/db/migration/new_scripts/V1057__add_elmis_code_column.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE interface_dataset
-    DROP COLUMN IF EXISTS elmiscode;
-
-ALTER TABLE interface_dataset
-    ADD COLUMN elmiscode character varying(100);
-
-create index msd_stock_status_facility_product on msd_stock_statuses (facilitycode, productcode);
