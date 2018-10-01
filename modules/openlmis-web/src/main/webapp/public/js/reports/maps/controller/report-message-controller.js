bf61cf1317bedb78d63a8fb94f6ac0f2b5872593
Elias Muluneh <eliasmu@gmail.com>
2018-10-01 11:54:16 -0400

    GE-31: fix error that prevented the popup from showing

--- modules/openlmis-web/src/main/webapp/public/js/reports/maps/controller/report-message-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/maps/controller/report-message-controller.js
@@ -10,7 +10,9 @@
  * You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-function ReportMessageController($scope, SendMessagesReportAttachment, GetFacilitySupervisors, SettingsByKey, ContactList, SendMessages, ReportingFacilityList, NonReportingFacilityList) {
+function ReportMessageController($scope, SendMessagesReportAttachment, GetFacilitySupervisors, SettingsByKey,
+                                 ContactList, SendMessages, ReportingFacilityList, NonReportingFacilityList,
+                                 messageService) {
 
   var allFacilities = [];
   // get configurations
