f2bfd9ff20a6d82683bf4ba7c3e5be61ef2b85d2
hassan <hhassan.developer@gmail.com>
2019-01-28 08:37:15 +0300

    Add select program by enabled-ivd form

--- modules/migration/src/main/resources/db/migration/new_scripts/V1067__update_master_column_configuration.sql
+++ modules/migration/src/main/resources/db/migration/new_scripts/V1067__update_master_column_configuration.sql
@@ -10,7 +10,7 @@ INSERT INTO public.vaccine_program_logistics_columns(
 WITH Q as (
 select * from vaccine_logistics_master_columns where name = 'totalAdjustedQuantity'
 )
-select (select id from programs where lower(code) = lower('VACCINE')  limit 1) programId,id mastercolumnid,label,displayOrder, true visible, 1 createdby, NOW() createddate, 1 modifiedby, NOW() modifieddate FROM Q;
+select (select id from programs where enableivdform = true  limit 1) programId,id mastercolumnid,label,displayOrder, true visible, 1 createdby, NOW() createddate, 1 modifiedby, NOW() modifieddate FROM Q;
  */
 
 --Additional Column in vaccine_logistics line Items
