7e39f4c81262a444e495da49e8579bc3579e6405
hassan <hhassan.developer@gmail.com>
2019-04-08 19:59:19 +0300

    Added vaccine targets migration script

--- modules/migration/src/main/resources/db/migration/new_scripts/V1071__add_vaccine_product_script.sql
+++ modules/migration/src/main/resources/db/migration/new_scripts/V1071__add_vaccine_product_script.sql
@@ -1,6 +1,6 @@
-Delete from vaccine_product_targets where productid in (2417,2429);
-INSERT INTO vaccine_product_targets (productid,targetwastagegood, targetwastagewarn, targetwastagebad, targetcoveragebad, targetcoveragewarn, targetcoveragegood, targetdropoutgood, targetdropoutwarn, targetdropoutbad, targetwastageclosedvialsgood, targetwastageclosedvialswarn, targetwastageclosedvialsbad) VALUES ('2417', '5', '5', '5', '50', '80', '90', '5', '10', '20', '5', '10', '20');
-INSERT INTO vaccine_product_targets (productid,targetwastagegood, targetwastagewarn, targetwastagebad, targetcoveragebad, targetcoveragewarn, targetcoveragegood, targetdropoutgood, targetdropoutwarn, targetdropoutbad, targetwastageclosedvialsgood, targetwastageclosedvialswarn, targetwastageclosedvialsbad) VALUES ('2429', '5', '5', '5', '50', '80', '90', '5', '10', '20', '5', '10', '20');
+--Delete from vaccine_product_targets where productid in (2417,2429);
+--INSERT INTO vaccine_product_targets (productid,targetwastagegood, targetwastagewarn, targetwastagebad, targetcoveragebad, targetcoveragewarn, targetcoveragegood, targetdropoutgood, targetdropoutwarn, targetdropoutbad, targetwastageclosedvialsgood, targetwastageclosedvialswarn, targetwastageclosedvialsbad) VALUES ('2417', '5', '5', '5', '50', '80', '90', '5', '10', '20', '5', '10', '20');
+--INSERT INTO vaccine_product_targets (productid,targetwastagegood, targetwastagewarn, targetwastagebad, targetcoveragebad, targetcoveragewarn, targetcoveragegood, targetdropoutgood, targetdropoutwarn, targetdropoutbad, targetwastageclosedvialsgood, targetwastageclosedvialswarn, targetwastageclosedvialsbad) VALUES ('2429', '5', '5', '5', '50', '80', '90', '5', '10', '20', '5', '10', '20');
 
 
 -- View: vw_vaccine_coverage
