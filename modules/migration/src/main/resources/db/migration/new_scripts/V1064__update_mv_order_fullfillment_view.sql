eeaa834cbb0a49416c6f5a11ce6ecb415a9b6417
ttesfaye <teklutesfayeh@gmail.com>
2019-05-29 11:58:34 +0200

    update mv_order_fullfillment materialized view

---
+++ modules/migration/src/main/resources/db/migration/new_scripts/V1064__update_mv_order_fullfillment_view.sql
@@ -0,0 +1,18 @@
+-- View: public.mv_order_fulfillment
+DROP MATERIALIZED VIEW if exists public.mv_order_fulfillment;
+
+CREATE MATERIALIZED VIEW public.mv_order_fulfillment
+TABLESPACE pg_default
+AS
+ SELECT shipment_line_items.orderid,
+    sum(COALESCE(shipment_line_items.quantityshipped, 0)) AS quantityshipped,
+    sum(COALESCE(shipment_line_items.substitutedproductquantityshipped, 0)) AS substitutedquantityshipped,
+    shipment_line_items.productcode,
+    shipment_line_items.packeddate,
+    shipment_line_items.shippeddate
+   FROM shipment_line_items
+  GROUP BY shipment_line_items.orderid, shipment_line_items.productcode, shipment_line_items.shippeddate, shipment_line_items.packeddate
+WITH DATA;
+
+ALTER TABLE public.mv_order_fulfillment
+    OWNER TO postgres;
