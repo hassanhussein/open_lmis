d743bbb8419bc2eb4d67ce1cf40feefb01fa1e9b
Elias Muluneh <eliasmu@gmail.com>
2019-06-11 17:23:30 -0400

    hide custom reports from showing up without specific permission

---
+++ modules/migration/src/main/resources/db/migration/new_scripts/V1061__create_new_rights.sql
@@ -0,0 +1,10 @@
+--VIEW_CUSTOM_REPORTS
+
+DELETE FROM rights where name = 'VIEW_CUSTOM_REPORTS';
+INSERT INTO rights (name, rightType, displaynamekey, displayOrder, description) VALUES
+('VIEW_CUSTOM_REPORTS','REPORT','View', 29,'Permission to view custom reports');
+
+
+DELETE FROM rights where name = 'VIEW_CUSTOM_REPORTS';
+INSERT INTO rights (name, rightType, displaynamekey, displayOrder, description) VALUES
+('VIEW_CUSTOM_REPORTS','REPORT','View', 29,'Permission to view custom reports');
