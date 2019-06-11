eec47942f8bb5499fd67979e029f5eec84a7d7b8
Elias Muluneh <eliasmu@gmail.com>
2019-06-11 18:22:58 -0400

    Remove duplicate right from migration script

--- modules/migration/src/main/resources/db/migration/new_scripts/V1066__create_new_rights.sql
+++ modules/migration/src/main/resources/db/migration/new_scripts/V1066__create_new_rights.sql
@@ -3,8 +3,3 @@
 DELETE FROM rights where name = 'VIEW_CUSTOM_REPORTS';
 INSERT INTO rights (name, rightType, displaynamekey, displayOrder, description) VALUES
 ('VIEW_CUSTOM_REPORTS','REPORT','View', 29,'Permission to view custom reports');
-
-
-DELETE FROM rights where name = 'VIEW_CUSTOM_REPORTS';
-INSERT INTO rights (name, rightType, displaynamekey, displayOrder, description) VALUES
-('VIEW_CUSTOM_REPORTS','REPORT','View', 29,'Permission to view custom reports');
