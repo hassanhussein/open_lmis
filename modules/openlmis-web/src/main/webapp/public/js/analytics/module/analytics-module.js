6c2d1940dcbb4ebb2b3004d3feae74181296cc7e
hassan <hhassan.developer@gmail.com>
2019-06-17 15:33:45 +0300

    update dashboard changes

--- modules/openlmis-web/src/main/webapp/public/js/analytics/module/analytics-module.js
+++ modules/openlmis-web/src/main/webapp/public/js/analytics/module/analytics-module.js
@@ -29,7 +29,7 @@ app.config(function($stateProvider, $urlRouterProvider, $breadcrumbProvider){
             controller:AnalyticsFunction,
             resolve:AnalyticsFunction.resolve,
             ncyBreadcrumb: {
-                label: 'Stock Chart'
+                label: 'Un-published Dashboard'
             }
         },
 
