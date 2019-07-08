19f3a1ca1293e0987421008f0ccfc3f8594c03e8
Ian Manyama <ian.manyama@tz-ghsc.com>
2019-07-08 14:16:03 +0300

    Add script to create new tb

--- modules/migration/src/main/resources/db/migration/new_scripts/V1087__new_tb_form_reference_data.sql
+++ modules/migration/src/main/resources/db/migration/new_scripts/V1087__new_tb_form_reference_data.sql
@@ -1,3 +1,9 @@
+INSERT INTO programs (code, name, description, active, templateConfigured, regimenTemplateConfigured, budgetingApplies, usesDar, push)
+  VALUES
+  ('TB&LEPROSY', 'TB AND LEPROSY', 'TB AND LEPROSY', TRUE, FALSE, FALSE, FALSE, FALSE, FALSE);
+
+
+
 INSERT INTO master_rnr_columns
 (   name                           ,  position  ,                         label                                  , source         , sourceConfigurable         , formula                                                  , indicator                                           , used       , visible       , mandatory    , description)
 VALUES
@@ -12,12 +18,24 @@ VALUES
 
 INSERT INTO program_rnr_columns
 (masterColumnId, programId, visible, source, position, label, rnrOptionId) VALUES
-((select id from master_rnr_columns where name='nextMonthPatient') , (select id from programs where code = 'TB&LEPROSY'),  true, 'C', 26,  'No of Patients on Treatment next month', null),
-((select id from master_rnr_columns where name='dosesPerMonth') , (select id from programs where code = 'TB&LEPROSY'),  true, 'R', 27,  'Individual Monthly Requirement', null),
-((select id from master_rnr_columns where name='totalRequirement') , (select id from programs where code = 'TB&LEPROSY'),  true, 'C', 28,  'Total Requirement', null),
-((select id from master_rnr_columns where name='totalQuantityNeededByHF') , (select id from programs where code = 'TB&LEPROSY'),  true, 'C', 29,  'Total Quantity Needed by HF', null),
-((select id from master_rnr_columns where name='quantityToIssue') , (select id from programs where code = 'TB&LEPROSY'),  true, 'C', 30,  'Quantity to Issue', null),
-((select id from master_rnr_columns where name='remarksForTBDispensedQuantity') , (select id from programs where code = 'TB&LEPROSY'),  true, 'U', 31,  'Remarks', null);
+((select id from master_rnr_columns where name='skipped'), (select id from programs where code = 'TB&LEPROSY'),  true, 'U', 1,  'Skip', null),
+((select id from master_rnr_columns where name='productCode'), (select id from programs where code = 'TB&LEPROSY'),  true, 'R', 2,  'MSD Code', null),
+((select id from master_rnr_columns where name='product'), (select id from programs where code = 'TB&LEPROSY'),  true, 'R', 3,  'Medicine Name', null),
+((select id from master_rnr_columns where name='dispensingUnit'), (select id from programs where code = 'TB&LEPROSY'),  true, 'R', 4,  'Unit of Measure (U)', null),
+((select id from master_rnr_columns where name='stockInHand'), (select id from programs where code = 'TB&LEPROSY'),  true, 'U', 5,  'Stock on Hand (A)', null),
+((select id from master_rnr_columns where name='lossesAndAdjustments'), (select id from programs where code = 'TB&LEPROSY'),  true, 'U', 6,  'Qnty damaged & Expired (B)', null),
+((select id from master_rnr_columns where name='nextMonthPatient'), (select id from programs where code = 'TB&LEPROSY'),  true, 'C', 7,  'No of Patients on Treatment next month', null),
+((select id from master_rnr_columns where name='dosesPerMonth'), (select id from programs where code = 'TB&LEPROSY'),  true, 'R', 8,  'Individual Monthly Requirement', null),
+((select id from master_rnr_columns where name='totalRequirement') , (select id from programs where code = 'TB&LEPROSY'),  true, 'C', 9,  'Total Requirement', null),
+((select id from master_rnr_columns where name='totalQuantityNeededByHF'), (select id from programs where code = 'TB&LEPROSY'),  true, 'C', 10,  'Total Quantity Needed by HF', null),
+((select id from master_rnr_columns where name='quantityToIssue'), (select id from programs where code = 'TB&LEPROSY'),  true, 'C', 11, 'Quantity to Issue (G) G = F - A', null),
+((select id from master_rnr_columns where name='total'), (select id from programs where code = 'TB&LEPROSY'),  true, 'C', 12, 'Quantity to issue  (converted to unit of measure) G/U', null),
+((select id from master_rnr_columns where name='quantityReceived'), (select id from programs where code = 'TB&LEPROSY'),  true, 'U', 13, 'Quantity Issued', null),
+((select id from master_rnr_columns where name='remarksForTBDispensedQuantity'), (select id from programs where code = 'TB&LEPROSY'),  true, 'U', 14,  'Remarks', null);
+
+
+
+update programs set templateConfigured = true where id in ((select id from programs where code = 'TB&LEPROSY'));
 
 
 
