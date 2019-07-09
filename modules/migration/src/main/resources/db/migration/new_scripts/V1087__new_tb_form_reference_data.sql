4d9ce17ab1dc0db3d8681367104b5f9c3071c503
Ian Manyama <ian.manyama@tz-ghsc.com>
2019-07-09 17:53:18 +0300

    Add program_products scripts

--- modules/migration/src/main/resources/db/migration/new_scripts/V1087__new_tb_form_reference_data.sql
+++ modules/migration/src/main/resources/db/migration/new_scripts/V1087__new_tb_form_reference_data.sql
@@ -26,8 +26,8 @@ INSERT INTO program_rnr_columns
 ((select id from master_rnr_columns where name='lossesAndAdjustments'), (select id from programs where code = 'TB&LEPROSY'),  true, 'U', 6,  'Qnty damaged & Expired (B)', null),
 ((select id from master_rnr_columns where name='nextMonthPatient'), (select id from programs where code = 'TB&LEPROSY'),  true, 'C', 7,  'No of Patients on Treatment next month', null),
 ((select id from master_rnr_columns where name='dosesPerMonth'), (select id from programs where code = 'TB&LEPROSY'),  true, 'R', 8,  'Individual Monthly Requirement', null),
-((select id from master_rnr_columns where name='totalRequirement') , (select id from programs where code = 'TB&LEPROSY'),  true, 'C', 9,  'Total Requirement', null),
-((select id from master_rnr_columns where name='totalQuantityNeededByHF'), (select id from programs where code = 'TB&LEPROSY'),  true, 'C', 10,  'Total Quantity Needed by HF', null),
+((select id from master_rnr_columns where name='totalRequirement') , (select id from programs where code = 'TB&LEPROSY'),  true, 'C', 9,  'Total Requirement (E) E = C * D', null),
+((select id from master_rnr_columns where name='totalQuantityNeededByHF'), (select id from programs where code = 'TB&LEPROSY'),  true, 'C', 10,  'Total Quantity Needed by HF (F) F ß= E * 2', null),
 ((select id from master_rnr_columns where name='quantityToIssue'), (select id from programs where code = 'TB&LEPROSY'),  true, 'C', 11, 'Quantity to Issue (G) G = F - A', null),
 ((select id from master_rnr_columns where name='total'), (select id from programs where code = 'TB&LEPROSY'),  true, 'C', 12, 'Quantity to issue  (converted to unit of measure) G/U', null),
 ((select id from master_rnr_columns where name='quantityReceived'), (select id from programs where code = 'TB&LEPROSY'),  true, 'U', 13, 'Quantity Issued', null),
@@ -193,5 +193,69 @@ ALTER TABLE products
 ADD COLUMN patientCalculationFormula VARCHAR(255);
 
 
---ALTER TABLE products
---ADD COLUMN individualMonthlyRequirement INTEGER;
+
+--Insert program_products
+insert into program_products(programId, productId, fullSupply, dosesPerMonth, currentPrice, active, displayOrder, productCategoryId) values
+((select id from programs where code='TB&LEPROSY'),(select id from products where code = '10010087AE'),'true',112,0.00,'true',455,(select id from product_categories where code='cgi')),
+((select id from programs where code='TB&LEPROSY'),(select id from products where code = '10010086AE'),'true',112,0.00,'true',0,(select id from product_categories where code='cgi')),
+((select id from programs where code='TB&LEPROSY'),(select id from products where code = '10010110AE'),'true',1,0,'true',9,(select id from product_categories where code='cgi')),
+((select id from programs where code='TB&LEPROSY'),(select id from products where code = '10010113AE'),'true',1,0,'true',356,(select id from product_categories where code='cgi'));
+((select id from programs where code='TB&LEPROSY'),(select id from products where code = '10010112AE'),'true',1,0,'true',356,(select id from product_categories where code='cgi'));
+((select id from programs where code='TB&LEPROSY'),(select id from products where code = '10010310AE'),'true',1,0,'true',356,(select id from product_categories where code='cgi'));
+((select id from programs where code='TB&LEPROSY'),(select id from products where code = '10010256AE'),'true',112,0,'true',356,(select id from product_categories where code='cgi'));
+((select id from programs where code='TB&LEPROSY'),(select id from products where code = '10020027AE'),'true',84,0,'true',356,(select id from product_categories where code='cgi'));
+((select id from programs where code='TB&LEPROSY'),(select id from products where code = '10010255AE'),'true',112,0,'true',356,(select id from product_categories where code='cgi'));
+((select id from programs where code='TB&LEPROSY'),(select id from products where code = '10010114AE'),'true',56,0,'true',356,(select id from product_categories where code='cgi'));
+((select id from programs where code='TB&LEPROSY'),(select id from products where code = '10010089AE'),'true',28,0,'true',356,(select id from product_categories where code='cgi'));
+
+
+
+--Program prodcut mapping
+
+--RHZE
+update  program_products set patientcalculationformula='ADULT_NEW_INTENSIVE_PHASE'
+where programid=(select id from programs where code='TB&LEPROSY') and productid=(select id from products where code='10010087AE')
+
+--RH 150/75
+update  program_products set patientcalculationformula='ADULT_NEW_CONTINUATION_PHASE'
+where programid=(select id from programs where code='TB&LEPROSY') and productid=(select id from products where code='10010086AE')
+
+
+--MB(Adult)
+update  program_products set patientcalculationformula='MB_ADULT'
+where programid=(select id from programs where code='TB&LEPROSY') and productid=(select id from products where code='10010110AE')
+
+--MB(Pedriatics)
+update  program_products set patientcalculationformula='MB_PEDIATRIC'
+where programid=(select id from programs where code='TB&LEPROSY') and productid=(select id from products where code='10010113AE')
+
+--PB(Adult)
+update  program_products set patientcalculationformula='PB_ADULT'
+where programid=(select id from programs where code='TB&LEPROSY') and productid=(select id from products where code='10010112AE')
+
+--PB(Pedriatics)
+update  program_products set patientcalculationformula='PB_PEDIATRIC'
+where programid=(select id from programs where code='TB&LEPROSY') and productid=(select id from products where code='10010310AE')
+
+
+--RHZ 75/50/150
+update  program_products set patientcalculationformula='CHILD_NEW_INTENSIVE_PHASE'
+where programid=(select id from programs where code='TB&LEPROSY') and productid=(select id from products where code='10010256AE')
+
+--Ethambutol 100mg
+update  program_products set patientcalculationformula='CHILD_NEW_INTENSIVE_PHASE'
+where programid=(select id from programs where code='TB&LEPROSY') and productid=(select id from products where code='10020027AE')
+
+
+--RH75/50
+update  program_products set patientcalculationformula='CHILD_NEW_CONTINUATION_PHASE'
+where programid=(select id from programs where code='TB&LEPROSY') and productid=(select id from products where code='10010255AE')
+
+--Isonizid 100mg
+update  program_products set patientcalculationformula='IPT_PEDIATRIC'
+where programid=(select id from programs where code='TB&LEPROSY') and productid=(select id from products where code='10010114AE')
+
+--Isonizid 300mg
+update  program_products set patientcalculationformula='IPT_ADULT'
+where programid=(select id from programs where code='TB&LEPROSY') and productid=(select id from products where code='10010089AE')
+
