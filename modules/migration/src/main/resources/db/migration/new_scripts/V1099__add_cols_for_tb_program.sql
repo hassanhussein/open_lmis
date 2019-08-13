e1d5a94922adf8cb8703eca4420c47bde2d0c3f3
Ian Manyama <ian.manyama@tz-ghsc.com>
2019-08-13 14:54:00 +0300

    Add skipped col on patient form

--- modules/migration/src/main/resources/db/migration/new_scripts/V1099__add_cols_for_tb_program.sql
+++ modules/migration/src/main/resources/db/migration/new_scripts/V1099__add_cols_for_tb_program.sql
@@ -2,3 +2,8 @@ ALTER TABLE patient_line_items
 ADD COLUMN skipped BOOLEAN;
 
 update patients set active=true where code='Number of Retreatment patients';
+
+
+
+INSERT INTO program_patient_columns(name, programId, label, visible, dataType, displayorder, fixed) values
+('monthOfTreatment',(select id from programs where code='TB&LEPROSY'), 'header.monthOfTreatment',true,'patient.reporting.dataType.numeric', 1, true),
