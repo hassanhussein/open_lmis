1f58b9c395f329bdbd3228267835e8a978f5d381
hassan <hhassan.developer@gmail.com>
2019-09-24 15:36:11 +0300

    Add permission right to manage lots

--- modules/core/src/test/java/org/openlmis/core/repository/mapper/RightMapperIT.java
+++ modules/core/src/test/java/org/openlmis/core/repository/mapper/RightMapperIT.java
@@ -96,7 +96,7 @@ public class RightMapperIT {
 
     List<Right> rights = rightMapper.getAll();
 
-    assertThat(rights.size(), is(124));
+    assertThat(rights.size(), is(109));
 
     assertThat(rights.get(0).getDisplayOrder(),is(1));
   }
