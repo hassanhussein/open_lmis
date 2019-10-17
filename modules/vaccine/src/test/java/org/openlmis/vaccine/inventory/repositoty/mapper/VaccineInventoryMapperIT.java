c5e91585f849fe6ae34eb78aa551c3ba1130556c
hassan <hhassan.developer@gmail.com>
2019-10-17 14:28:47 +0300

    un-comment vaccine inventory tests

--- modules/vaccine/src/test/java/org/openlmis/vaccine/inventory/repositoty/mapper/VaccineInventoryMapperIT.java
+++ modules/vaccine/src/test/java/org/openlmis/vaccine/inventory/repositoty/mapper/VaccineInventoryMapperIT.java
@@ -6,8 +6,7 @@
  *  
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
  * You should have received a copy of the GNU Affero General Public License along with this program.  If not, see http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
- *//*
-
+ */
 
 package org.openlmis.vaccine.inventory.repositoty.mapper;
 
@@ -91,4 +90,3 @@ public class VaccineInventoryMapperIT {
 
 
 }
-*/
