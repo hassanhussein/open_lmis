1ace26ff421af24276c25c04d46bb6929790c599
hassan <hhassan.developer@gmail.com>
2020-01-15 09:19:41 +0300

    Batch processing conf

---
+++ modules/openlmis-web/src/main/webapp/public/js/reports/order/controller/detail-controlller.js
@@ -0,0 +1,5 @@
+function OrderFillRateFunc($scope){
+
+
+
+}
--- modules/openlmis-web/src/main/webapp/public/js/reports/order/controller/order-fill-rate-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/order/controller/order-fill-rate-controller.js
@@ -1,39 +1,20 @@
-function OrderFillRateControllerFunc($scope,GeoDistrictTree,AggregateItemFillRateSummary) {
+function OrderFillRateControllerFunc($scope,GeoDistrictTree,AggregateItemFillRateSummary,$timeout,AggregateItemFillRateByOrder) {
 
-$scope.data = 'mamamama';
-
-
-
-/*
-    GeoDistrictTree.get({}, function (data) {
-
-        var data2 = data.regionFacilityTree;
-        console.log(data2);
-        $('#tree').treeview({
-            data: data2,
-            levels: 2,
-            color: "#398085",
-            onhoverColor: 'lightblue',
-            onNodeSelected: function (event, data) {
-                getDataForDisplay(data);
-            }
-
-        });
-
-    });*/
-
-      var calculateAverageItemFillRate = function(orderList){
+      $scope.calculateAverageItemFillRate = function(orderList){
       var sum = 0;
       for(var i=0; i< orderList.length;i++) {
        sum += parseInt((orderList[i].receivedQuantity/orderList[i].approvedQuantity )*100,10);
       }
-       return sum/ orderList.length;
+       var result = sum/ orderList.length;
+       return Math.round(result);
       };
 
     var filter = {};
 
     filter.schedule=2; filter.period=91; filter.year=2019; filter.program=1; filter.max=10000;filter.page=1; filter.limit=10000;
 
+     $scope.dataRow =$scope.filteredFacilityData =[];
+     $scope.filter = filter;
       $scope.OnFilterChanged = function() {
 
         $scope.data = $scope.datarows = [];
@@ -41,30 +22,69 @@ $scope.data = 'mamamama';
         $scope.filter.page = 1;
         $scope.filter.limit = 10000;*/
 
+        var filteredFacilityData = [];
+
         AggregateItemFillRateSummary.get(filter, function(data) {
 
+           filteredFacilityData = data.openLmisResponse.rows;
 
-           var groupByFacilityData = _.groupBy(data.openLmisResponse.rows, 'facilityName');
+           var groupByDistrictData = _.groupBy(data.openLmisResponse.rows, 'district');
 
-           var mappedData = _.map(groupByFacilityData, function(orders, index){
+           var districtMappedData = _.map(groupByDistrictData, function(orders, index){
 
-               return {"facilityName":index,"district":orders[0].district,"region":orders[0].region,"msdZone":orders[0].msdZone,"nodes":orders,"orf":calculateAverageItemFillRate(orders) };
-
+           var mapFacilities = _.map(orders, function(dt, ind){
+              return {'text':dt.facilityName+' : <strong>Aggregate Order Fill Rate</strong>: <strong>'+$scope.calculateAverageItemFillRate([dt])+'%</strong>','orderId':dt.rnrId,'facilityName':dt.facilityName}
            });
 
-           //    console.log(mappedData);
+           var facilities  = _.each(orders, function(data){
+           return {'text':data.facilityName, 'nodes':[]}
+           });
 
-           $scope.dataRow = mappedData;
+           return {'level':1,'text':index+' : <strong>Aggregate Order Fill Rate</strong>:<strong>'+$scope.calculateAverageItemFillRate(facilities)+'%</strong>', 'nodes':mapFacilities, 'region':facilities[0].region,'msdZone':facilities[0].msdZone,'total':$scope.calculateAverageItemFillRate(facilities)};
 
-                                console.log($scope.dataRow);
+          });
+
+          var regionData = _.groupBy(districtMappedData,'region');
+
+          var mapRegion = _.map(regionData, function(regions, index) {
+
+               var sum = 0;
+               _.each(regions, function(data){
+                 sum += data.total;
+               });
+              return {'level':2,'text':index+' : <strong>Aggregate Order Fill Rate</strong>:<strong>'+Math.round(sum/regions.length)+'%</strong>', 'nodes':regions, 'msdZone':regions[0].msdZone, 'total': Math.round(sum/regions.length)}
+          });
+
+          var groupByZone = _.groupBy(mapRegion, 'msdZone');
+
+          var mapZone = _.map(groupByZone, function(zones, index){
+
+                    var sum = 0;
+                               _.each(zones, function(data){
+                                 sum += data.total;
+                               });
+
+               return {'level':3,'text':index+' : <strong>Aggregate Order Fill Rate</strong>:<strong>'+Math.round(sum/zones.length)+'%</strong>', 'nodes':zones, 'total': Math.round(sum/zones.length)};
+
+          });
+
+
+
+
+
+
+              console.log(mapZone);
+
+           //$scope.dataRow = mappedData;
+
 
                                   $('#tree').treeview({
-                                            data: mappedData,
-                                            levels: 2,
+                                            data: mapZone,
+                                            levels: 4,
                                             color: "#398085",
                                             onhoverColor: 'lightblue',
                                             onNodeSelected: function (event, data) {
-                                                getDataForDisplay(data);
+                                                     $scope.getDataForDisplay(data, filteredFacilityData);
                                             }
 
                                         });
@@ -88,7 +108,71 @@ $scope.data = 'mamamama';
         });
       };
 
-$scope.OnFilterChanged();
+       $scope.OnFilterChanged();
+
+        $scope.mappedFacilities = [];
+
+       $scope.getDataForDisplay = function (data, allData){
+
+       $timeout(function(){
+
+           if(data.level === undefined) {
+
+
+               var filterData = _.where(allData, {facilityName: data.facilityName});
+
+               var groupByFacility = _.groupBy(filterData, 'facilityName');
+
+              var mappedD = _.map(groupByFacility, function(data,index) {
+
+                 return {'facilityName':index, 'district':data[0].district, 'msdZone': data[0].msdZone, 'orderList':data};
+               });
+
+              $scope.mappedFacilities = mappedD;
+               $scope.$apply();
+                        console.log($scope.mappedFacilities);
+
+               }
+
+
+       },100);
+
+
+
+        }
+
+
+
+  $scope.getDetails = function(rnr, row){
+
+    console.log($scope.filter);
+
+   var param = angular.extend($scope.filter, {'orderId':parseInt(rnr,10)});
+
+   AggregateItemFillRateByOrder.get(param, function(data) {
+     row.items = data.openLmisResponse.rows;
+     $scope.allTheData = row;
+
+
+
+        if (data.openLmisResponse !== undefined && data.openLmisResponse.rows !== undefined) {
+          console.log(data);
+           $scope.pagination = data.openLmisResponse.pagination;
+           $scope.totalItems = 1000;
+           $scope.currentPage = $scope.pagination.page;
+           //$scope.tableParams.total = $scope.totalItems;
+           //check if this is last page and reduce totalItemSize so user can not go to next page
+           if (data.openLmisResponse.rows.length !== $scope.pageSize) {
+           $scope.totalItems = $scope.pageSize * $scope.page;
+           }
+           $scope.data = data.openLmisResponse.rows;
+          // $scope.paramsChanged($scope.tableParams);
+          }
+
+
+   });
+   $scope.successModal = true;
+  };
 
 
 }
