3f864518292cb03335eaf91e38b45fc067d77155
Yona Mahundi <yona_mahundi@tz.jsi.com>
2020-02-25 17:20:17 +0300

    added more document types

---
+++ modules/migration/src/main/resources/db/migration/new_scripts/V1117__insert_document_types_migration.sql
@@ -0,0 +1,14 @@
+
+
+
+
+
+INSERT INTO public.document_types(
+            name, description, createdby, createddate, modifiedby, modifieddate)
+    VALUES ('Inspection Report', 'For insepction report', 307, NOW(), 307, NOW()),
+    ('Packing List', 'This is packinglist', 307, NOW(), 307, NOW()),
+    ('Receiving Committee Report', 'rcr', 307, NOW(), 307, NOW()),
+    ('Cold Chain Monitor Card', 'rcr', 307, NOW(), 307, NOW()),
+    ('Cost Estimate', 'rcr', 307, NOW(), 307, NOW()),
+    ('Delivery Note', 'rcr', 307, NOW(), 307, NOW()),
+    ('VAR Report', 'This is certs', 307, NOW(), 307, NOW());
