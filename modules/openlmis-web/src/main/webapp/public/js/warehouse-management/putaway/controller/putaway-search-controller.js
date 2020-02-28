6fe564b464301f2ed7a3807385941d1a0c9ad583
Yona Mahundi <yona_mahundi@tz.jsi.com>
2020-02-28 14:23:26 +0300

    putway ui and status change on inspection

--- modules/openlmis-web/src/main/webapp/public/js/warehouse-management/putaway/controller/putaway-search-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/warehouse-management/putaway/controller/putaway-search-controller.js
@@ -7,11 +7,101 @@
  *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
  *  You should have received a copy of the GNU Affero General Public License along with this program.  If not, see http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org.
  */
+function PutawaySearchController($scope, Receive, $location, navigateBackService, $dialog){
 
-function PutawaySearchController($scope, $location) {
-  var today = new Date();
-  $scope.referenceNo='IVD/SM/'+today.getFullYear()+'/'+today.getMonth()+'/'+today.getTime();
-  $scope.referenceNoA='IVD/SA/'+today.getFullYear()+'/'+today.getMonth()+'/'+today.getTime();
+  $scope.searchOptions = [
+    {value: "poNumber", name: "PO Number"},
+
+  ];
+
+  $scope.$parent.putawayViewMode=false;
+
+
+  $scope.showResults = false;
+  $scope.currentPage = 1;
+  $scope.selectedSearchOption = navigateBackService.selectedSearchOption || $scope.searchOptions[0];
+
+  $scope.selectSearchType = function (searchOption) {
+    $scope.selectedSearchOption = searchOption;
+  };
+
+
+ // column to sort
+ $scope.column = 'poNumber';
+
+ // sort ordering (Ascending or Descending). Set true for desending
+ $scope.reverse = false;
+
+ // called on header click
+ $scope.sortColumn = function(col){
+  $scope.column = col;
+  if($scope.reverse){
+   $scope.reverse = false;
+   $scope.reverseclass = 'arrow-up';
+  }else{
+   $scope.reverse = true;
+   $scope.reverseclass = 'arrow-down';
+  }
+ };
+
+ // remove and change class
+ $scope.sortClass = function(col){
+  if($scope.column == col ){
+   if($scope.reverse){
+    return 'arrow-down';
+   }else{
+    return 'arrow-up';
+   }
+  }else{
+   return '';
+  }
+ };
+
+
+  $scope.$on('$viewContentLoaded', function () {
+    $scope.query = navigateBackService.query;
+  });
+
+
+
+  $scope.$watch('currentPage', function () {
+    if ($scope.currentPage !== 0)
+      $scope.search($scope.currentPage, $scope.searchedQuery);
+  });
+
+  $scope.search = function (page, lastQuery) {
+    if (!($scope.query || lastQuery)) return;
+    lastQuery ? getPreadvice(page, lastQuery) : getPreadvice(page, $scope.query);
+  };
+
+  function getPreadvice(page, query) {
+    query = query.trim();
+    $scope.searchedQuery = query;
+    Receive.get({"searchParam": $scope.searchedQuery, "column": $scope.selectedSearchOption.value, "page": page}, function (data) {
+      $scope.asnList = data.receives;
+      console.log($scope.asnList);
+      $scope.pagination = data.pagination;
+      $scope.totalItems = $scope.pagination.totalRecords;
+      $scope.currentPage = $scope.pagination.page;
+      $scope.showResults = true;
+      $scope.showSearchResults = true;
+    }, {});
+  }
+
+  $scope.clearSearch = function () {
+    $scope.query = "";
+    $scope.totalItems = 0;
+    $scope.asnList = [];
+    $scope.showResults = false;
+    angular.element("#searchPreadive").focus();
+  };
+
+
+  $scope.triggerSearch = function (event) {
+    if (event.keyCode === 13) {
+      $scope.search(1);
+    }
+  };
+
 
 }
-
