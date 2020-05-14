9a92d44756b7b31cf03b484d96ead52de2755e24
hassan <hhassan.developer@gmail.com>
2020-05-14 14:29:44 +0300

    update stock ledger

--- modules/openlmis-web/src/main/java/org/openlmis/web/controller/warehouse/StockMovementController.java
+++ modules/openlmis-web/src/main/java/org/openlmis/web/controller/warehouse/StockMovementController.java
@@ -11,6 +11,7 @@ import org.openlmis.vaccine.service.warehouse.TransferService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -61,7 +62,7 @@ public class StockMovementController extends BaseController {
     }
 
 
-    @RequestMapping(value = "/get-current-stock/{facilityId}", method = GET, headers = ACCEPT_JSON)
+    @RequestMapping(value = "/get-current-stock", method = GET, headers = ACCEPT_JSON)
     public ResponseEntity<OpenLmisResponse> getCurrentStockOnHand(HttpServletRequest request) {
 
         Long userId = loggedInUserId(request);
