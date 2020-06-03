41bdca52c1a27851174eb43ad358982d35fb4453
hassan <hhassan.developer@gmail.com>
2020-06-03 09:35:09 +0300

    Add notification icon

---
+++ modules/migration/src/main/resources/db/migration/new_scripts/V1154__add_right_permission_for_so_notification.sql
@@ -0,0 +1,4 @@
+DELETE FROM rights WHERE name = 'VIEW_STOCK_OUT_NOTIFICATION_MENU';
+
+INSERT INTO rights (name, rightType, displaynamekey, description, displayOrder) VALUES
+  ('VIEW_STOCK_OUT_NOTIFICATION_MENU','REPORT','right.manage.stock.out.notification.menu','Permission to view Stock out notification menu', 37);
