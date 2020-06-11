8cebf907b33dd1fc84c84c3723ccb7ba7dbad848
ttesfaye <teklutesfayeh@gmail.com>
2020-06-11 11:03:00 +0300

    fix query

--- modules/migration/src/main/resources/db/migration/new_scripts/V1097__setting_emergency_order_dashlet.sql
+++ modules/migration/src/main/resources/db/migration/new_scripts/V1097__setting_emergency_order_dashlet.sql
@@ -1,5 +1,5 @@
-delete from configuration_settings where key = ''DB_DASHLETS_EMERGECNCY_ORDER_ANALYSIS'';
+delete from configuration_settings where key = 'DB_DASHLETS_EMERGECNCY_ORDER_ANALYSIS';
 
 insert into configuration_settings(key, value, name, description, groupname, valuetype, isconfigurable)
-values(''DB_DASHLETS_EMERGECNCY_ORDER_ANALYSIS'',''http://insights2.zm-elmis.org/public/dashboard/11c4e784-eb24-4f0a-81fa-f78deedf8f75'',
-''Emergency Order Analysis'',''Emergency Order Analysis dashlet'',''Main Dashboard Dashlets'',''TEXT'',true);
+values('DB_DASHLETS_EMERGECNCY_ORDER_ANALYSIS','http://insights2.zm-elmis.org/public/dashboard/11c4e784-eb24-4f0a-81fa-f78deedf8f75',
+'Emergency Order Analysis','Emergency Order Analysis dashlet','Main Dashboard Dashlets','TEXT',true);
