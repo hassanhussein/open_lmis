111eb107a5f4baf2b6004960340f6b9e68e2bfa5
Mchunguzi <felix_koseph@tz.jsi.com>
2020-07-20 15:32:52 +0300

    Stock adjustments API

--- modules/report/src/main/java/org/openlmis/report/mapper/LotsOnHandMapper.java
+++ modules/report/src/main/java/org/openlmis/report/mapper/LotsOnHandMapper.java
@@ -193,7 +193,8 @@ public interface LotsOnHandMapper {
 
 
     @Select("select vvmst.name as vvm,lhl.id,s.facilityid as facilityId,lo.lotnumber as lotNumber,wh.name as warehouseName,lo.expirationdate as expirationDate,s.productid as productId," +
-            "(h.quantityOnHand-(select sum(quantity) from vaccine_distribution_line_item_lots where lotid=lo.id and lotNumber=lo.lotNumber limit 1))  totalQuantityOnHand ,s.effectivedate as effectiveDate,s.modifieddate as modifiedDate ,pr.primaryname as fullName,wl.name  as locationName from lot_on_hand_locations lhl " +
+            "(h.quantityOnHand-(select sum(quantity) from vaccine_distribution_line_item_lots where lotid=lo.id and lotNumber=lo.lotNumber limit 1))  totalQuantityOnHand ," +
+            "s.effectivedate as effectiveDate,s.modifieddate as modifiedDate ,pr.primaryname as fullName,wl.name  as locationName from lot_on_hand_locations lhl " +
             "left join lots_on_hand h on (lhl.lotonhandid = h.id) " +
             "left join  stock_cards s on (s.id = h.stockcardid) " +
             "left join lots lo on(lo.id=h.lotid) "+
