2ac304ec4285a07860af80806c99c8757a5e73f7
hassan <hhassan.developer@gmail.com>
2020-07-21 17:04:09 +0300

    add add job to push LLIN data

--- modules/core/src/main/java/org/openlmis/core/service/ELMISInterfaceService.java
+++ modules/core/src/main/java/org/openlmis/core/service/ELMISInterfaceService.java
@@ -112,6 +112,11 @@ public class ELMISInterfaceService {
 
     }
 
+    @Scheduled(cron = "${batch.job.quantification.extract}")
+    public void refreshQuantificationExtract() {
+
+        repository.refreshViewsByName("mv_quantification_extraction");
+    }
     // @Scheduled(cron = "${batch.job.send.bed.net.data}")
     // @Scheduled(fixedRate = 900000)
     public void processMosquitoNetReportingData() {
