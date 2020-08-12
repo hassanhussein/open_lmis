9de83466d48590bc92aaec668ee791eedc2ef1ab
Mchunguzi <felix_koseph@tz.jsi.com>
2020-08-12 11:26:34 +0300

    documents uploads

---
+++ modules/migration/src/main/resources/db/migration/new_scripts/V1144__add_document_column.sql
@@ -0,0 +1,26 @@
+ALTER TABLE documents
+ADD COLUMN IF NOT EXISTS deleted BOOLEAN  default false;
+
+ALTER TABLE public.documents
+    ADD COLUMN IF NOT EXISTS createddate timestamp without time zone;
+
+    ALTER TABLE public.documents
+    ADD COLUMN IF NOT EXISTS createdby integer;
+
+    ALTER TABLE public.documents
+    ADD COLUMN IF NOT EXISTS comment character varying(500);
+
+
+    ALTER TABLE public.documents
+    ADD COLUMN IF NOT EXISTS deletionlocation character varying(40);
+
+    ALTER TABLE public.documents
+    ADD COLUMN IF NOT EXISTS deletedby integer;
+
+    ALTER TABLE public.purchase_documents
+    ADD COLUMN IF NOT EXISTS deleted boolean DEFAULT false;
+
+    ALTER TABLE public.purchase_documents
+    ADD COLUMN IF NOT EXISTS comment character varying(200);
+
+
