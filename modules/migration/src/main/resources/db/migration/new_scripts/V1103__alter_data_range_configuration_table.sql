6d6c3098a7eb1b74996c21f70863168081e2f5d2
ttesfaye <teklutesfayeh@gmail.com>
2020-08-31 16:06:35 +0300

    add range column to data_range_configuration table

---
+++ modules/migration/src/main/resources/db/migration/new_scripts/V1103__alter_data_range_configuration_table.sql
@@ -0,0 +1,10 @@
+ALTER TABLE public.data_range_flags_configuration
+ADD COLUMN if not exists range numrange NOT NULL;
+	
+ALTER TABLE  public.data_range_flags_configuration  
+DROP CONSTRAINT IF EXISTS data_range_flags_configuration_range_excl;
+
+ALTER TABLE public.data_range_flags_configuration
+ADD CONSTRAINT  data_range_flags_configuration_range_excl EXCLUDE USING gist (
+       category with=, range WITH &&
+       );
