8c16cf12af22005130a1661f918f3c613636fde0
Mchunguzi <felix_koseph@tz.jsi.com>
2020-09-02 10:02:41 +0300

    GRN unique

--- modules/vaccine/src/main/java/org/openlmis/vaccine/repository/mapper/warehouse/receive/ReceiveMapper.java
+++ modules/vaccine/src/main/java/org/openlmis/vaccine/repository/mapper/warehouse/receive/ReceiveMapper.java
@@ -164,7 +164,7 @@ public interface ReceiveMapper {
     @Select(" SELECT * FROM fn_create_inpsection(#{id}::int) ")
     Integer updateInspection(@Param("id") Long id);
 
-    @Select("SELECT receiveNumber from receive_line_Items order by id desc limit 1")
+    @Select("SELECT receiveNumber from receive_line_Items where receiveNumber is not null order by id desc limit 1")
     String getLastReceiptNumber();
 
 
