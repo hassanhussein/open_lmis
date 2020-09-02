8d396963ec131a2421acc437e1151601a9e9a62b
iannashin <iannashin@gmail.com>
2020-09-02 14:23:43 +0300

    Zanzibar Dashboard - Fix bugs

--- modules/openlmis-web/src/main/webapp/public/js/analytics/controller/analytics-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/analytics/controller/analytics-controller.js
@@ -5,12 +5,10 @@ function AnalyticsFunction(notifications,$stateParams, GetEmergencyAndRegularRnR
     //resourceLoadingConfig.hideReloadIcon = true;
     //resourceLoadingConfig.loadingDashlet = [];
 
-    $scope.stockAvailabilityDashboardUrl = $sce.trustAsResourceUrl("https://dashboard.tz.elmis-dev.org/embed/dashboard/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZXNvdXJjZSI6eyJkYXNoYm9hcmQiOjI5fSwicGFyYW1zIjp7fSwiaWF0IjoxNTk4MjYxOTIxfQ.DL5Bg84TX9piTpkPV7qWK4fuZGUi8-mV6akR9c106EA?tracer=true#bordered&titled");
+    $scope.stockAvailabilityDashboardUrl = $sce.trustAsResourceUrl("http://dashboard.tz.elmis-dev.org/embed/dashboard/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZXNvdXJjZSI6eyJkYXNoYm9hcmQiOjI5fSwicGFyYW1zIjp7fSwiaWF0IjoxNTk4MjYxOTIxfQ.DL5Bg84TX9piTpkPV7qWK4fuZGUi8-mV6akR9c106EA?tracer=true#bordered&titled");
     $scope.requisitionsDashboardUrl = $sce.trustAsResourceUrl("http://dashboard.tz.elmis-dev.org/embed/dashboard/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZXNvdXJjZSI6eyJkYXNoYm9hcmQiOjMwfSwicGFyYW1zIjp7fSwiaWF0IjoxNTk4MjU1ODQ2fQ.ZRgC14-wKY8KD9CO0keABm6aNozR9kkRG3m99yzmibQ#bordered=true&titled=true");
     $scope.reportingUrl = $sce.trustAsResourceUrl("http://dashboard.tz.elmis-dev.org/embed/dashboard/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZXNvdXJjZSI6eyJkYXNoYm9hcmQiOjMxfSwicGFyYW1zIjp7fSwiaWF0IjoxNTk4MjU2MTQzfQ.OefMgzh8s56T3N8i-B5un-H3BnJwto8dwPAVLO4kHE0#bordered=true&titled=true");
     $scope.notifications = notifications;
-
-
 
 
     var params;
