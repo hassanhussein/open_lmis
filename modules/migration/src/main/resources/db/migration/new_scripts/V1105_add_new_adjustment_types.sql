340d1ae19b03a46da5f72b091e1cc4c1dd4ace62
ttesfaye <teklutesfayeh@gmail.com>
2020-09-29 10:53:16 +0300

    ECZ-204 Add new Adjustment Types to UI

---
+++ modules/migration/src/main/resources/db/migration/new_scripts/V1105_add_new_adjustment_types.sql
@@ -0,0 +1,4 @@
+INSERT INTO losses_adjustments_types (name, description, additive, displayOrder) VALUES
+('RECALLED','Recalled'	,false	,2),
+('QUARANTINED', 'Quarantined'	,FALSE,	3),
+('UNQUARANTINED', 'Unquarantined',	TRUE,	9);
