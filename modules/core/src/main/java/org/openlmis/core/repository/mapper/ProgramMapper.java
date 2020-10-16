dac3314d5d4eb82d54e724a537b5d01171057758
ttesfaye <teklutesfayeh@gmail.com>
2020-10-16 14:14:54 +0300

    update program push attribute

--- modules/core/src/main/java/org/openlmis/core/repository/mapper/ProgramMapper.java
+++ modules/core/src/main/java/org/openlmis/core/repository/mapper/ProgramMapper.java
@@ -24,164 +24,165 @@ import java.util.List;
 @Repository
 public interface ProgramMapper {
 
-  @Insert({"INSERT INTO programs(code, name, description, active, push, templateConfigured, regimenTemplateConfigured)",
-    "VALUES (#{code}, #{name}, #{description}, #{active}, #{push}, #{templateConfigured}, #{regimenTemplateConfigured})"})
-  @Options(useGeneratedKeys = true)
-  Integer insert(Program program);
+    @Insert({"INSERT INTO programs(code, name, description, active, push, templateConfigured, regimenTemplateConfigured)",
+            "VALUES (#{code}, #{name}, #{description}, #{active}, #{push}, #{templateConfigured}, #{regimenTemplateConfigured})"})
+    @Options(useGeneratedKeys = true)
+    Integer insert(Program program);
 
-  @Delete("DELETE FROM programs WHERE code=#{code}")
-  public void deleteByCode(String code);
+    @Delete("DELETE FROM programs WHERE code=#{code}")
+    public void deleteByCode(String code);
 
-  @Select({"SELECT P.*",
-    "FROM programs P, programs_supported PS",
-    "WHERE P.id = PS.programId AND",
-    "PS.facilityId = #{facilityId} AND",
-    "PS.active = true AND",
-    "P.active = true"})
-  List<Program> getActiveByFacility(Long facilityId);
+    @Select({"SELECT P.*",
+            "FROM programs P, programs_supported PS",
+            "WHERE P.id = PS.programId AND",
+            "PS.facilityId = #{facilityId} AND",
+            "PS.active = true AND",
+            "P.active = true"})
+    List<Program> getActiveByFacility(Long facilityId);
 
-  @Select("SELECT * FROM programs WHERE push = FALSE ORDER BY name ")
-  List<Program> getAllPullPrograms();
+    @Select("SELECT * FROM programs WHERE push = FALSE ORDER BY name ")
+    List<Program> getAllPullPrograms();
 
-  @Select("SELECT * FROM programs WHERE push = TRUE ORDER BY name ")
-  List<Program> getAllPushPrograms();
+    @Select("SELECT * FROM programs WHERE push = TRUE ORDER BY name ")
+    List<Program> getAllPushPrograms();
 
-  @Select({"SELECT",
-    "p.id AS id,",
-    "p.code AS code,",
-    "p.name AS name,",
-    "p.description AS description,",
-    "ps.active AS active",
-    "FROM programs p, programs_supported ps WHERE",
-    "p.id = ps.programId AND",
-    "ps.facilityId = #{facilityId}"})
-  List<Program> getByFacilityId(Long facilityId);
+    @Select({"SELECT",
+            "p.id AS id,",
+            "p.code AS code,",
+            "p.name AS name,",
+            "p.description AS description,",
+            "ps.active AS active",
+            "FROM programs p, programs_supported ps WHERE",
+            "p.id = ps.programId AND",
+            "ps.facilityId = #{facilityId}"})
+    List<Program> getByFacilityId(Long facilityId);
 
-  @Select("SELECT id FROM programs WHERE LOWER(code) = LOWER(#{code})")
-  Long getIdForCode(String code);
+    @Select("SELECT id FROM programs WHERE LOWER(code) = LOWER(#{code})")
+    Long getIdForCode(String code);
 
-  @Select("SELECT * FROM programs WHERE id = #{id}")
-  Program getById(Long id);
+    @Select("SELECT * FROM programs WHERE id = #{id}")
+    Program getById(Long id);
 
-  @Select({"SELECT DISTINCT p.*",
-    "FROM programs p",
-    "INNER JOIN role_assignments ra ON p.id = ra.programId",
-    "INNER JOIN role_rights rr ON ra.roleId = rr.roleId",
-    "WHERE ra.userId = #{userId}",
-    "AND rr.rightName = ANY (#{commaSeparatedRights}::VARCHAR[])",
-    "AND ra.supervisoryNodeId IS NOT NULL",
-    "AND p.active = TRUE",
-    "AND p.push = FALSE"})
-  List<Program> getUserSupervisedActivePrograms(@Param(value = "userId") Long userId,
-                                                @Param(value = "commaSeparatedRights") String commaSeparatedRights);
+    @Select({"SELECT DISTINCT p.*",
+            "FROM programs p",
+            "INNER JOIN role_assignments ra ON p.id = ra.programId",
+            "INNER JOIN role_rights rr ON ra.roleId = rr.roleId",
+            "WHERE ra.userId = #{userId}",
+            "AND rr.rightName = ANY (#{commaSeparatedRights}::VARCHAR[])",
+            "AND ra.supervisoryNodeId IS NOT NULL",
+            "AND p.active = TRUE",
+            "AND p.push = FALSE"})
+    List<Program> getUserSupervisedActivePrograms(@Param(value = "userId") Long userId,
+                                                  @Param(value = "commaSeparatedRights") String commaSeparatedRights);
 
-  @Select({"SELECT DISTINCT p.*",
-    "FROM programs p",
-    "INNER JOIN role_assignments ra ON p.id = ra.programId",
-    "INNER JOIN role_rights rr ON ra.roleId = rr.roleId",
-    "WHERE ra.userId = #{userId}",
-    "AND rr.rightName = ANY (#{commaSeparatedRights}::VARCHAR[])",
-    "AND ra.supervisoryNodeId IS NOT NULL",
-    "AND p.active = TRUE " ,
-    "AND p.enableIvdForm = TRUE ",
-    "AND p.push = FALSE"})
-  List<Program> getUserSupervisedActiveIvdPrograms(@Param(value = "userId") Long userId,
-                                                @Param(value = "commaSeparatedRights") String commaSeparatedRights);
-
-
-  @Select({"SELECT DISTINCT p.* ",
-    "FROM programs p",
-    "INNER JOIN programs_supported ps ON p.id = ps.programId",
-    "INNER JOIN role_assignments ra ON ra.programId = p.id",
-    "INNER JOIN role_rights rr ON rr.roleId = ra.roleId",
-    "WHERE ra.supervisoryNodeId IS NULL",
-    "AND p.active = TRUE",
-    "AND p.push = FALSE",
-    "AND ps.active= TRUE",
-    "AND ra.userId = #{userId}",
-    "AND ps.facilityId = #{facilityId}",
-    "AND rr.rightName = ANY(#{commaSeparatedRights}::VARCHAR[])"})
-  List<Program> getProgramsSupportedByUserHomeFacilityWithRights(@Param("facilityId") Long facilityId,
-                                                                 @Param("userId") Long userId,
-                                                                 @Param("commaSeparatedRights") String commaSeparatedRights);
-
-  @Select({"SELECT DISTINCT p.* ",
-    "FROM programs p",
-    "INNER JOIN programs_supported ps ON p.id = ps.programId",
-    "INNER JOIN role_assignments ra ON ra.programId = p.id",
-    "INNER JOIN role_rights rr ON rr.roleId = ra.roleId",
-    "WHERE ra.supervisoryNodeId IS NULL",
-    "AND p.active = TRUE",
-    "AND p.push = FALSE",
-    "AND ps.active= TRUE",
-    "AND ra.userId = #{userId}",
-    "AND p.enableIvdForm = TRUE ",
-    "AND ps.facilityId = #{facilityId}",
-    "AND rr.rightName = ANY(#{commaSeparatedRights}::VARCHAR[])"})
-  List<Program> getIvdProgramsSupportedByUserHomeFacilityWithRights(@Param("facilityId") Long facilityId,
-                                                                 @Param("userId") Long userId,
-                                                                 @Param("commaSeparatedRights") String commaSeparatedRights);
+    @Select({"SELECT DISTINCT p.*",
+            "FROM programs p",
+            "INNER JOIN role_assignments ra ON p.id = ra.programId",
+            "INNER JOIN role_rights rr ON ra.roleId = rr.roleId",
+            "WHERE ra.userId = #{userId}",
+            "AND rr.rightName = ANY (#{commaSeparatedRights}::VARCHAR[])",
+            "AND ra.supervisoryNodeId IS NOT NULL",
+            "AND p.active = TRUE ",
+            "AND p.enableIvdForm = TRUE ",
+            "AND p.push = FALSE"})
+    List<Program> getUserSupervisedActiveIvdPrograms(@Param(value = "userId") Long userId,
+                                                     @Param(value = "commaSeparatedRights") String commaSeparatedRights);
 
 
-  @Select({"SELECT DISTINCT p.*",
-    "FROM programs p",
-    "INNER JOIN role_assignments ra ON p.id = ra.programId",
-    "INNER JOIN role_rights rr ON ra.roleId = rr.roleId",
-    "WHERE ra.userId = #{userId}",
-    "AND rr.rightName = ANY (#{commaSeparatedRights}::VARCHAR[])",
-    "AND p.active = true"})
-  List<Program> getActiveProgramsForUserWithRights(@Param(value = "userId") Long userId,
-                                                   @Param(value = "commaSeparatedRights") String commaSeparatedRights);
+    @Select({"SELECT DISTINCT p.* ",
+            "FROM programs p",
+            "INNER JOIN programs_supported ps ON p.id = ps.programId",
+            "INNER JOIN role_assignments ra ON ra.programId = p.id",
+            "INNER JOIN role_rights rr ON rr.roleId = ra.roleId",
+            "WHERE ra.supervisoryNodeId IS NULL",
+            "AND p.active = TRUE",
+            "AND p.push = FALSE",
+            "AND ps.active= TRUE",
+            "AND ra.userId = #{userId}",
+            "AND ps.facilityId = #{facilityId}",
+            "AND rr.rightName = ANY(#{commaSeparatedRights}::VARCHAR[])"})
+    List<Program> getProgramsSupportedByUserHomeFacilityWithRights(@Param("facilityId") Long facilityId,
+                                                                   @Param("userId") Long userId,
+                                                                   @Param("commaSeparatedRights") String commaSeparatedRights);
+
+    @Select({"SELECT DISTINCT p.* ",
+            "FROM programs p",
+            "INNER JOIN programs_supported ps ON p.id = ps.programId",
+            "INNER JOIN role_assignments ra ON ra.programId = p.id",
+            "INNER JOIN role_rights rr ON rr.roleId = ra.roleId",
+            "WHERE ra.supervisoryNodeId IS NULL",
+            "AND p.active = TRUE",
+            "AND p.push = FALSE",
+            "AND ps.active= TRUE",
+            "AND ra.userId = #{userId}",
+            "AND p.enableIvdForm = TRUE ",
+            "AND ps.facilityId = #{facilityId}",
+            "AND rr.rightName = ANY(#{commaSeparatedRights}::VARCHAR[])"})
+    List<Program> getIvdProgramsSupportedByUserHomeFacilityWithRights(@Param("facilityId") Long facilityId,
+                                                                      @Param("userId") Long userId,
+                                                                      @Param("commaSeparatedRights") String commaSeparatedRights);
 
 
-  @Update("UPDATE programs SET templateConfigured = true WHERE id = #{id}")
-  void setTemplateConfigured(Long id);
-
-  @Select({"SELECT DISTINCT p.* FROM programs p INNER JOIN programs_supported ps ON p.id = ps.programId",
-    "INNER JOIN role_assignments ra ON ra.programId = p.id",
-    "INNER JOIN role_rights rr ON rr.roleId = ra.roleId",
-    "AND p.active = TRUE",
-    "AND ps.active= TRUE",
-    "AND ra.userId = #{userId}",
-    "AND ps.facilityId = #{facilityId}",
-    "AND rr.rightName = ANY(#{commaSeparatedRights}::VARCHAR[])"})
-  List<Program> getProgramsForUserByFacilityAndRights(@Param("facilityId") Long facilityId,
-                                                      @Param("userId") Long userId,
-                                                      @Param("commaSeparatedRights") String commaSeparatedRights);
-
-  @Select("SELECT * FROM programs ORDER BY templateConfigured DESC, name")
-  List<Program> getAll();
+    @Select({"SELECT DISTINCT p.*",
+            "FROM programs p",
+            "INNER JOIN role_assignments ra ON p.id = ra.programId",
+            "INNER JOIN role_rights rr ON ra.roleId = rr.roleId",
+            "WHERE ra.userId = #{userId}",
+            "AND rr.rightName = ANY (#{commaSeparatedRights}::VARCHAR[])",
+            "AND p.active = true"})
+    List<Program> getActiveProgramsForUserWithRights(@Param(value = "userId") Long userId,
+                                                     @Param(value = "commaSeparatedRights") String commaSeparatedRights);
 
 
-  @Select("SELECT * FROM programs WHERE LOWER(code) = LOWER(#{code})")
-  Program getByCode(String code);
+    @Update("UPDATE programs SET templateConfigured = true WHERE id = #{id}")
+    void setTemplateConfigured(Long id);
 
-  @Select("SELECT * FROM programs ORDER BY regimenTemplateConfigured DESC, name")
-  List<Program> getAllByRegimenTemplate();
+    @Select({"SELECT DISTINCT p.* FROM programs p INNER JOIN programs_supported ps ON p.id = ps.programId",
+            "INNER JOIN role_assignments ra ON ra.programId = p.id",
+            "INNER JOIN role_rights rr ON rr.roleId = ra.roleId",
+            "AND p.active = TRUE",
+            "AND ps.active= TRUE",
+            "AND ra.userId = #{userId}",
+            "AND ps.facilityId = #{facilityId}",
+            "AND rr.rightName = ANY(#{commaSeparatedRights}::VARCHAR[])"})
+    List<Program> getProgramsForUserByFacilityAndRights(@Param("facilityId") Long facilityId,
+                                                        @Param("userId") Long userId,
+                                                        @Param("commaSeparatedRights") String commaSeparatedRights);
 
-  @Update("UPDATE programs SET regimenTemplateConfigured = true WHERE id = #{id}")
-  void setRegimenTemplateConfigured(Long id);
+    @Select("SELECT * FROM programs ORDER BY templateConfigured DESC, name")
+    List<Program> getAll();
 
-  @Update("UPDATE programs SET sendFeed = #{sendFeed} WHERE code = #{program.code}")
-  void setFeedSendFlag(@Param("program") Program program, @Param("sendFeed") Boolean sendFeed);
 
-  @Select("SELECT * FROM programs WHERE sendFeed = TRUE")
-  List<Program> getProgramsForNotification();
+    @Select("SELECT * FROM programs WHERE LOWER(code) = LOWER(#{code})")
+    Program getByCode(String code);
 
-  @Update("UPDATE programs SET " +
-    "code = #{code}, name = #{name}, " +
-    "isEquipmentConfigured = #{isEquipmentConfigured}, " +
-    "showNonFullSupplyTab = #{showNonFullSupplyTab}, " +
-    "hideSkippedProducts = #{hideSkippedProducts}, " +
-    "enableSkipPeriod = #{enableSkipPeriod}," +
-    "enableIvdForm = #{enableIvdForm},  " +
-    "budgetingApplies = #{budgetingApplies}, " +
-    "usepriceschedule = #{usePriceSchedule}" +
-    "WHERE id = #{id}")
-  void update(Program program);
+    @Select("SELECT * FROM programs ORDER BY regimenTemplateConfigured DESC, name")
+    List<Program> getAllByRegimenTemplate();
 
-  @Select("SELECT * FROM programs " +
-    " where enableIvdForm = true " +
-    " order by name")
-  List<Program> getAllIvdPrograms();
+    @Update("UPDATE programs SET regimenTemplateConfigured = true WHERE id = #{id}")
+    void setRegimenTemplateConfigured(Long id);
+
+    @Update("UPDATE programs SET sendFeed = #{sendFeed} WHERE code = #{program.code}")
+    void setFeedSendFlag(@Param("program") Program program, @Param("sendFeed") Boolean sendFeed);
+
+    @Select("SELECT * FROM programs WHERE sendFeed = TRUE")
+    List<Program> getProgramsForNotification();
+
+    @Update("UPDATE programs SET " +
+            "code = #{code}, name = #{name}, " +
+            "isEquipmentConfigured = #{isEquipmentConfigured}, " +
+            "showNonFullSupplyTab = #{showNonFullSupplyTab}, " +
+            "hideSkippedProducts = #{hideSkippedProducts}, " +
+            "enableSkipPeriod = #{enableSkipPeriod}," +
+            "enableIvdForm = #{enableIvdForm},  " +
+            "budgetingApplies = #{budgetingApplies}, " +
+            "push =#{push}," +
+            "usepriceschedule = #{usePriceSchedule}" +
+            "WHERE id = #{id}")
+    void update(Program program);
+
+    @Select("SELECT * FROM programs " +
+            " where enableIvdForm = true " +
+            " order by name")
+    List<Program> getAllIvdPrograms();
 }
