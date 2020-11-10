b6f6aee1aed1355f5079d358510da71794725d18
ttesfaye <teklutesfayeh@gmail.com>
2020-11-10 13:31:43 +0300

    fix emergency request report

--- modules/report/src/main/java/org/openlmis/report/builder/EmergencyRequestQueryBuilder.java
+++ modules/report/src/main/java/org/openlmis/report/builder/EmergencyRequestQueryBuilder.java
@@ -20,27 +20,31 @@ public class EmergencyRequestQueryBuilder {
 
     private static String getQueryString(NonReportingFacilityParam filterParam) {
         BEGIN();
-        SELECT_DISTINCT("r.rnrid");
+        SELECT_DISTINCT("r.id as rnrid");
         SELECT("r.status");
         SELECT("r.programid");
-        SELECT("r.zoneid");
-        SELECT("r.provinceid");
-        SELECT("r.districtid");
-        SELECT("r.facilityid");
+        SELECT("d.zone_id zoneid");
+        SELECT(" d.region_id provinceid");
+        SELECT("d.district_id districtid");
+        SELECT(" r.facilityid");
         SELECT("r.periodid");
 
-        SELECT("r.program");
+        SELECT(" p.name \"program\"");
 //        SELECT_DISTINCT("r.zone");
-        SELECT("r.province");
-        SELECT("r.district");
-        SELECT("r.facility");
-        SELECT("r.facilitycode");
-        SELECT("r.period");
-        SELECT("r.startdate");
-        SELECT("r.enddate");
+        SELECT(" d.region_name province");
+        SELECT("d.district_name district");
+        SELECT("f.name as facility");
+        SELECT("f.code as facilitycode");
+        SELECT("pp.name asperiod");
+        SELECT("pp.startdate");
+        SELECT("pp.enddate");
         SELECT(" o.ordernumber");
-        FROM("mv_requisition r");
-        LEFT_OUTER_JOIN( " orders o on r.rnrid =o.id");
+        FROM("requisitions r");
+        INNER_JOIN("facilities f on r.facilityid=f.id");
+        INNER_JOIN("vw_districts d on  f.geographiczoneid=d.district_id");
+        INNER_JOIN("programs p on p.id= r.programid");
+        INNER_JOIN("processing_periods pp on pp.id =r.periodid");
+        LEFT_OUTER_JOIN( " orders o on r.id =o.id");
         WHERE("emergency = TRUE");
         writePredicates(filterParam);
         String query = SQL();
@@ -51,9 +55,9 @@ public class EmergencyRequestQueryBuilder {
 
         WHERE(programIsFilteredBy("r.programid"));
         if (filterParams.getZone() != 0) {
-            WHERE("(r.zoneid =#{filterCriteria.zone}" +
-                    " or r.districtid= #{filterCriteria.zone} or " +
-                    " r.provinceid= #{filterCriteria.zone})");
+            WHERE("( d.zone_id  =#{filterCriteria.zone}" +
+                    " or d.district_id= #{filterCriteria.zone} or " +
+                    " d.region_id= #{filterCriteria.zone})");
         }
         if (filterParams.getPeriod() != 0) {
             WHERE(periodIsFilteredBy("r.periodid"));
