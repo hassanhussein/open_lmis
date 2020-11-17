2cc9c08f5588da70e91b70f9c7bddf5ae954ec41
Yona Mahundi <yona_mahundi@tz.jsi.com>
2020-11-17 12:05:58 +0300

    to bin location validation

--- modules/migration/src/main/resources/db/migration/new_scripts/V1158__add_transfer_reasons.sql
+++ modules/migration/src/main/resources/db/migration/new_scripts/V1158__add_transfer_reasons.sql
@@ -1,7 +1,7 @@
-INSERT INTO public.wms_reasons (
-id, code, name) VALUES (
-'2'::integer, 'REASON TWO'::text, 'Coldroom full'::text);
-
- INSERT INTO public.wms_reasons (
- id, code, name) VALUES (
- '1'::integer, 'REASON ONE'::text, 'Coldroom failure'::text);
+--INSERT INTO public.wms_reasons (
+--id, code, name) VALUES (
+--'2'::integer, 'REASON TWO'::text, 'Coldroom full'::text);
+--
+-- INSERT INTO public.wms_reasons (
+-- id, code, name) VALUES (
+-- '1'::integer, 'REASON ONE'::text, 'Coldroom failure'::text);
