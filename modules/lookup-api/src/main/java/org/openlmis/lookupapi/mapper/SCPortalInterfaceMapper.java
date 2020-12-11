166b8dfe4189fd4ecfa01cf7b767cc1ea71d8f71
hassan <hhassan.developer@gmail.com>
2020-12-11 19:47:50 +0300

    Fix covid 19 error code

--- modules/lookup-api/src/main/java/org/openlmis/lookupapi/mapper/SCPortalInterfaceMapper.java
+++ modules/lookup-api/src/main/java/org/openlmis/lookupapi/mapper/SCPortalInterfaceMapper.java
@@ -23,16 +23,17 @@ public interface SCPortalInterfaceMapper {
             "")
     List<HashMap<String, Object>> getAllProducts();
 
-    @Select(" SELECT f.hfrcode as \"facility_id\", productcode as \"product_code\"\n" +
-            ", 4 as \"level\", " +
-            " CASE WHEN stockinhand is null then 0 else stockinhand::text END as \"quantity\", to_char(r.modifieddate, 'yyyy-MM-dd') as \"updated_at\" \n" +
-            "from requisitions r\n" +
-            "JOIN requisition_line_items i on r.id = i.rnrid\n" +
-            "JOIN facilities F on r.facilityId = F.ID" +
-            " JOIN processing_periods per ON r.periodiD = per.id" +
-            " where f.code::text is not null\n" +
-            "               and  f.hfrcode::text not in ('.','-') " +
-            " ORDER BY R.ID DESC LIMIT 1000\n" +
+    @Select(
+            "    SELECT f.hfrcode as facility_id, productcode as product_code\n" +
+                    "            , 4 as level, \n" +
+                    "            CASE WHEN stockinhand is null then 0::text else stockinhand::text END as quantity, to_char(r.modifieddate, 'yyyy-MM-dd') as updated_at\n" +
+                    "           from requisitions r\n" +
+                    "            JOIN requisition_line_items i on r.id = i.rnrid\n" +
+                    "            JOIN facilities F on r.facilityId = F.ID\n" +
+                    "             JOIN processing_periods per ON r.periodiD = per.id\n" +
+                    "             where f.code::text is not null\n" +
+                    "                           and  f.hfrcode::text not in ('.','-') \n" +
+                    "             ORDER BY R.ID DESC LIMIT 1000"+
           //  " and per.startDate>= #{startDate}::DATE and per.endDate <=#{endDate}::DATE" +
 
             "")
@@ -64,7 +65,7 @@ public interface SCPortalInterfaceMapper {
     List<HashMap<String, Object>> getWastages(@Param("startDate") String startDate, @Param("endDate") String endDate);
 
     @Select(" SELECT f.hfrcode as facility_id, productcode as \"product_code\",\n" +
-            "          CASE WHEN amc is null THEN 0 ELSE amc::text END as \"actual_consumed\",\n" +
+            "          CASE WHEN amc is null THEN 0::TEXT ELSE amc::text END as \"actual_consumed\",\n" +
             "                      to_char(pp.enddate,'YYYY-MM-dd') as period,\n" +
             "           normalizedconsumption::text as \"forecast_consumed\"\n" +
             "            from requisitions r\n" +
