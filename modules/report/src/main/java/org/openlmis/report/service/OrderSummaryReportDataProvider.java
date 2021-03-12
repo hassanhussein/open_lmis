6769d3a6ee9439490398b85f25a0050685d0a543
iannashin <iannashin@gmail.com>
2021-03-12 13:37:41 +0300

    Disable timezone configuration since the server has moved to Tanzania

--- modules/report/src/main/java/org/openlmis/report/service/OrderSummaryReportDataProvider.java
+++ modules/report/src/main/java/org/openlmis/report/service/OrderSummaryReportDataProvider.java
@@ -37,7 +37,6 @@ import org.openlmis.rnr.service.RequisitionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -50,193 +49,190 @@ import java.util.*;
 @NoArgsConstructor
 public class OrderSummaryReportDataProvider extends ReportDataProvider {
 
-    public static final String REGION = "REGION";
-    public static final String DISTRICT = "DISTRICT";
-    public static final String FAX = "FAX";
-    public static final String PHONE = "PHONE";
-    public static final String FACILITY_ADDRESS = "FACILITY_ADDRESS";
-    public static final String APPROVED_DATE = "APPROVED_DATE";
-    public static final String APPROVED_BY = "APPROVED_BY";
-    public static final String IN_APPROVAL_DATE = "IN_APPROVAL_DATE";
-    public static final String IN_APPROVAL_BY = "IN_APPROVAL_BY";
-    public static final String AUTHORIZED_DATE = "AUTHORIZED_DATE";
-    public static final String AUTHORIZED_BY = "AUTHORIZED_BY";
-    public static final String ORDER_NO = "ORDER_NO";
-    public static final String ADDRESS = "ADDRESS";
-    public static final String CUSTOM_REPORT_TITLE = "CUSTOM_REPORT_TITLE";
+  public static final String REGION = "REGION";
+  public static final String DISTRICT = "DISTRICT";
+  public static final String FAX = "FAX";
+  public static final String PHONE = "PHONE";
+  public static final String FACILITY_ADDRESS = "FACILITY_ADDRESS";
+  public static final String APPROVED_DATE = "APPROVED_DATE";
+  public static final String APPROVED_BY = "APPROVED_BY";
+  public static final String IN_APPROVAL_DATE = "IN_APPROVAL_DATE";
+  public static final String IN_APPROVAL_BY = "IN_APPROVAL_BY";
+  public static final String AUTHORIZED_DATE = "AUTHORIZED_DATE";
+  public static final String AUTHORIZED_BY = "AUTHORIZED_BY";
+  public static final String ORDER_NO = "ORDER_NO";
+  public static final String ADDRESS = "ADDRESS";
+  public static final String CUSTOM_REPORT_TITLE = "CUSTOM_REPORT_TITLE";
 
-    public static final String PROGRAM = "PROGRAM";
-    public static final String SCHEDULE = "SCHEDULE";
-    public static final String YEAR = "YEAR";
-    public static final String PERIOD = "PERIOD";
-    public static final String DEPOT = "DEPOT";
-    public static final String TYPE = "TYPE";
-    public static final String RELEASED_BY = "RELEASED_BY";
-    public static final String RELEASED_DATE = "RELEASED_DATE";
+  public static final String PROGRAM = "PROGRAM";
+  public static final String SCHEDULE = "SCHEDULE";
+  public static final String YEAR = "YEAR";
+  public static final String PERIOD = "PERIOD";
+  public static final String DEPOT = "DEPOT";
+  public static final String TYPE = "TYPE";
+  public static final String RELEASED_BY = "RELEASED_BY";
+  public static final String RELEASED_DATE = "RELEASED_DATE";
 
-    @Autowired
-    private OrderSummaryReportMapper reportMapper;
+  @Autowired
+  private OrderSummaryReportMapper reportMapper;
 
-    @Autowired
-    private OrderService orderService;
+  @Autowired
+  private OrderService orderService;
 
-    @Autowired
-    private ConfigurationSettingService configurationService;
+  @Autowired
+  private ConfigurationSettingService configurationService;
 
-    @Autowired
-    private ReportLookupService reportLookupService;
+  @Autowired
+  private ReportLookupService reportLookupService;
 
-    @Autowired
-    private RequisitionService requistionService;
+  @Autowired
+  private RequisitionService requistionService;
 
-    @Autowired
-    private ProcessingPeriodService periodService;
+  @Autowired
+  private ProcessingPeriodService periodService;
 
-    @Autowired
-    private ProcessingScheduleService scheduleSerive;
+  @Autowired
+  private ProcessingScheduleService scheduleSerive;
 
-    @Autowired
-    private FacilityService facilityService;
+  @Autowired
+  private FacilityService facilityService;
 
+  @Autowired
+  private ProgramService programService;
 
-    @Autowired
-    private ProgramService programService;
+  @Autowired
+  ConfigurationSettingService settingService;
 
-    @Autowired
-    ConfigurationSettingService settingService;
+  @Override
+  public List<? extends ResultRow> getReportBody(Map<String, String[]> filterCriteria, Map<String, String[]> sortCriteria, int page, int pageSize) {
+    RowBounds rowBounds = new RowBounds((page - 1) * pageSize, pageSize);
+    OrderReportParam orderReportParam = getReportFilterData(filterCriteria);
+    orderReportParam.setTitle(getTitle(orderReportParam));
 
-    @Override
-    public List<? extends ResultRow> getReportBody(Map<String, String[]> filterCriteria, Map<String, String[]> sortCriteria, int page, int pageSize) {
-        RowBounds rowBounds = new RowBounds((page - 1) * pageSize, pageSize);
-        OrderReportParam orderReportParam = getReportFilterData(filterCriteria);
-        orderReportParam.setTitle(getTitle(orderReportParam));
+    List<SourceOfFunds> sourceOfFundsList = this.requistionService.getSourceOfFundsBy(orderReportParam.getOrderId());
 
-        List<SourceOfFunds> sourceOfFundsList = this.requistionService.getSourceOfFundsBy(orderReportParam.getOrderId());
+    List<OrderSummaryReport> summaryReport = new ArrayList<>();
 
-        List<OrderSummaryReport> summaryReport = new ArrayList<>();
+    List<OrderSummaryReport> summaryReport1 = reportMapper.getOrderSummaryReport(orderReportParam, sortCriteria, rowBounds);
 
-        List<OrderSummaryReport> summaryReport1 = reportMapper.getOrderSummaryReport(orderReportParam, sortCriteria, rowBounds);
-
-        for(OrderSummaryReport report: summaryReport1){
-            if(!sourceOfFundsList.isEmpty()) {
-                report.setSourceOfFundList(sourceOfFundsList);
-            }
-            summaryReport.add(report);
-        }
-
-        return summaryReport;
+    for (OrderSummaryReport report : summaryReport1) {
+      if (!sourceOfFundsList.isEmpty()){
+        report.setSourceOfFundList(sourceOfFundsList);
+      }
+      summaryReport.add(report);
     }
 
-    private String getTitle(OrderReportParam orderReportParam) {
-        if(orderReportParam.getOrderId() == null || orderReportParam.getOrderId() == 0)
-            return "";
+    return summaryReport;
+  }
 
-        Rnr rnr = requistionService.getLWById(orderReportParam.getOrderId());
-        Program program = programService.getById(rnr.getProgram().getId());
-        ProcessingPeriod period = periodService.getById(rnr.getPeriod().getId());
+  private String getTitle(OrderReportParam orderReportParam) {
+    if (orderReportParam.getOrderId() == null || orderReportParam.getOrderId() == 0)
+      return "";
 
-        StringBuilder title = new StringBuilder("");
-        title.append("Program: ")
-                .append(program.getName())
-                .append("\n").
-                append("Period: ")
-                .append(period.getName())
-                .append(" - ")
-                .append(period.getStringYear());
-        return title.toString();
+    Rnr rnr = requistionService.getLWById(orderReportParam.getOrderId());
+    Program program = programService.getById(rnr.getProgram().getId());
+    ProcessingPeriod period = periodService.getById(rnr.getPeriod().getId());
+
+    StringBuilder title = new StringBuilder("");
+    title.append("Program: ")
+            .append(program.getName())
+            .append("\n").
+            append("Period: ")
+            .append(period.getName())
+            .append(" - ")
+            .append(period.getStringYear());
+    return title.toString();
+  }
+
+  private OrderReportParam getReportFilterData(Map<String, String[]> filterCriteria) {
+    OrderReportParam orderReportParam = ParameterAdaptor.parse(filterCriteria, OrderReportParam.class);
+    if (orderReportParam.getOrderId() <= 0){
+      orderReportParam.setOrderId(reportMapper.getRequisitionId(orderReportParam.getFacility(), orderReportParam.getProgram(), orderReportParam.getPeriod()));
+    }
+    return orderReportParam;
+  }
+
+  @Override
+  public String getFilterSummary(Map<String, String[]> params) {
+    return getReportFilterData(params).toString();
+  }
+
+  @Override
+  public Map<String, String> getExtendedHeader(Map filterCriteria) {
+    OrderReportParam orderReportParam = getReportFilterData(filterCriteria);
+    Map<String, String> result = new HashMap<>();
+    result.put(ADDRESS, configurationService.getConfigurationStringValue(ConfigurationSettingKey.ORDER_REPORT_ADDRESS));
+    result.put(CUSTOM_REPORT_TITLE, configurationService.getConfigurationStringValue(ConfigurationSettingKey.ORDER_REPORT_TITLE));
+    result.put(ConfigurationSettingKey.ORDER_SUMMARY_SHOW_SIGNATURE_SPACE_FOR_CUSTOMER, configurationService.getConfigurationStringValue(ConfigurationSettingKey.ORDER_SUMMARY_SHOW_SIGNATURE_SPACE_FOR_CUSTOMER));
+    result.put(ConfigurationSettingKey.ORDER_SUMMARY_SHOW_DISCREPANCY_SECTION, configurationService.getConfigurationStringValue(ConfigurationSettingKey.ORDER_SUMMARY_SHOW_DISCREPANCY_SECTION));
+
+    Rnr rnr = requistionService.getLWById(orderReportParam.getOrderId());
+
+    Program program = programService.getById(rnr.getProgram().getId());
+    ProcessingPeriod period = this.periodService.getById(rnr.getPeriod().getId());
+    ProcessingSchedule schedule = this.scheduleSerive.get(period.getScheduleId());
+    Facility depotFacility = null;
+    String supplyDepot = StringHelper.getValue(filterCriteria, "supplyDepot");
+    if (supplyDepot != null)
+      depotFacility = this.facilityService.getFacilityById(Long.parseLong(supplyDepot));
+    String year = StringHelper.getValue(filterCriteria, "year");
+
+    String type = rnr.isEmergency() ? "Emergency" : "Regular";
+
+    String orderNo = orderService.getOrderNumberConfiguration().getOrderNumberFor(rnr.getId(), program, rnr.isEmergency());
+    result.put(ORDER_NO, orderNo);
+    // rnr.getSupplyingDepotId().
+
+    result.put(PROGRAM, program.getName());
+    if (year != null)
+      result.put(YEAR, year);
+    result.put(SCHEDULE, schedule.getName());
+    result.put(PERIOD, period.getName());
+    if (depotFacility != null)
+      result.put(DEPOT, depotFacility.getName());
+    result.put(TYPE, type);
+
+    final SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yy hh:mm a");
+
+    TimeZone timeZone = TimeZone.getTimeZone(configurationService.getByKey(Constants.LOCAL_TIME_SETTINGS).getValue());
+
+    dateFormat.setTimeZone(timeZone);
+
+    List<RequisitionStatusChange> changes = reportMapper.getLastUsersWhoActedOnRnr(orderReportParam.getOrderId(), RnrStatus.AUTHORIZED.name());
+    if (!changes.isEmpty()){
+      result.put(AUTHORIZED_BY, changes.get(0).getCreatedBy().getFirstName() + " " + changes.get(0).getCreatedBy().getLastName());
+      result.put(AUTHORIZED_DATE, dateFormat.format(changes.get(0).getCreatedDate()));
     }
 
-    private OrderReportParam getReportFilterData(Map<String, String[]> filterCriteria) {
-        OrderReportParam orderReportParam = ParameterAdaptor.parse(filterCriteria, OrderReportParam.class);
-        if (orderReportParam.getOrderId() <= 0) {
-            orderReportParam.setOrderId(reportMapper.getRequisitionId(orderReportParam.getFacility(), orderReportParam.getProgram(), orderReportParam.getPeriod()));
-        }
-        return orderReportParam;
+    changes = reportMapper.getLastUsersWhoActedOnRnr(orderReportParam.getOrderId(), RnrStatus.IN_APPROVAL.name());
+    if (!changes.isEmpty()){
+      result.put(IN_APPROVAL_BY, changes.get(0).getCreatedBy().getFirstName() + " " + changes.get(0).getCreatedBy().getLastName());
+      result.put(IN_APPROVAL_DATE, dateFormat.format(changes.get(0).getCreatedDate()));
     }
 
-    @Override
-    public String getFilterSummary(Map<String, String[]> params) {
-        return getReportFilterData(params).toString();
+    changes = reportMapper.getLastUsersWhoActedOnRnr(orderReportParam.getOrderId(), RnrStatus.APPROVED.name());
+    if (!changes.isEmpty()){
+      result.put(APPROVED_BY, changes.get(0).getCreatedBy().getFirstName() + " " + changes.get(0).getCreatedBy().getLastName());
+      result.put(APPROVED_DATE, dateFormat.format(changes.get(0).getCreatedDate()));
     }
 
-    @Override
-    public Map<String, String> getExtendedHeader(Map filterCriteria) {
-        OrderReportParam orderReportParam = getReportFilterData(filterCriteria);
-        Map<String, String> result = new HashMap<>();
-        result.put(ADDRESS, configurationService.getConfigurationStringValue(ConfigurationSettingKey.ORDER_REPORT_ADDRESS));
-        result.put(CUSTOM_REPORT_TITLE, configurationService.getConfigurationStringValue(ConfigurationSettingKey.ORDER_REPORT_TITLE));
-        result.put(ConfigurationSettingKey.ORDER_SUMMARY_SHOW_SIGNATURE_SPACE_FOR_CUSTOMER, configurationService.getConfigurationStringValue(ConfigurationSettingKey.ORDER_SUMMARY_SHOW_SIGNATURE_SPACE_FOR_CUSTOMER));
-        result.put(ConfigurationSettingKey.ORDER_SUMMARY_SHOW_DISCREPANCY_SECTION, configurationService.getConfigurationStringValue(ConfigurationSettingKey.ORDER_SUMMARY_SHOW_DISCREPANCY_SECTION));
+    changes = reportMapper.getLastUsersWhoActedOnRnr(orderReportParam.getOrderId(), RnrStatus.RELEASED.name());
+    if (!changes.isEmpty()){
+      result.put(RELEASED_BY, changes.get(0).getCreatedBy().getFirstName() + " " + changes.get(0).getCreatedBy().getLastName());
+      result.put(RELEASED_DATE, dateFormat.format(changes.get(0).getCreatedDate()));
+    }
+    // register the facility related parameters here
+    Facility currentFacility = reportLookupService.getFacilityForRnrId(orderReportParam.getOrderId());
+    result.put(FACILITY_ADDRESS, currentFacility.getAddress1());
+    result.put(PHONE, currentFacility.getMainPhone());
+    result.put(FAX, currentFacility.getFax());
+    result.put(DISTRICT, currentFacility.getGeographicZone().getName());
+    if (currentFacility.getGeographicZone() != null){
+      result.put(REGION, currentFacility.getGeographicZone().getParent().getName());
+    }
 
-
-        Rnr rnr = requistionService.getLWById(orderReportParam.getOrderId());
-
-        Program program = programService.getById(rnr.getProgram().getId());
-        ProcessingPeriod period = this.periodService.getById(rnr.getPeriod().getId());
-        ProcessingSchedule schedule = this.scheduleSerive.get(period.getScheduleId());
-        Facility depotFacility = null;
-        String supplyDepot = StringHelper.getValue(filterCriteria, "supplyDepot");
-        if (supplyDepot != null)
-            depotFacility = this.facilityService.getFacilityById(Long.parseLong(supplyDepot));
-        String year = StringHelper.getValue(filterCriteria, "year");
-
-        String type = rnr.isEmergency() ? "Emergency" : "Regular";
-
-        String orderNo = orderService.getOrderNumberConfiguration().getOrderNumberFor(rnr.getId(), program, rnr.isEmergency());
-        result.put(ORDER_NO, orderNo);
-        // rnr.getSupplyingDepotId().
-
-
-        result.put(PROGRAM, program.getName());
-        if (year != null)
-            result.put(YEAR, year);
-        result.put(SCHEDULE, schedule.getName());
-        result.put(PERIOD, period.getName());
-        if (depotFacility != null)
-            result.put(DEPOT, depotFacility.getName());
-        result.put(TYPE, type);
-
-        final SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yy hh:mm a");
-
-        TimeZone timeZone = TimeZone.getTimeZone(configurationService.getByKey(Constants.LOCAL_TIME_SETTINGS).getValue());
-
-
-
-        List<RequisitionStatusChange> changes = reportMapper.getLastUsersWhoActedOnRnr(orderReportParam.getOrderId(), RnrStatus.AUTHORIZED.name());
-        if (!changes.isEmpty()) {
-            result.put(AUTHORIZED_BY, changes.get(0).getCreatedBy().getFirstName() + " " + changes.get(0).getCreatedBy().getLastName());
-            result.put(AUTHORIZED_DATE, dateFormat.format(changes.get(0).getCreatedDate()));
-        }
-
-        changes = reportMapper.getLastUsersWhoActedOnRnr(orderReportParam.getOrderId(), RnrStatus.IN_APPROVAL.name());
-        if (!changes.isEmpty()) {
-            result.put(IN_APPROVAL_BY, changes.get(0).getCreatedBy().getFirstName() + " " + changes.get(0).getCreatedBy().getLastName());
-            result.put(IN_APPROVAL_DATE, dateFormat.format(changes.get(0).getCreatedDate()));
-        }
-
-        changes = reportMapper.getLastUsersWhoActedOnRnr(orderReportParam.getOrderId(), RnrStatus.APPROVED.name());
-        if (!changes.isEmpty()) {
-            result.put(APPROVED_BY, changes.get(0).getCreatedBy().getFirstName() + " " + changes.get(0).getCreatedBy().getLastName());
-            result.put(APPROVED_DATE, dateFormat.format(changes.get(0).getCreatedDate()));
-        }
-
-        changes = reportMapper.getLastUsersWhoActedOnRnr(orderReportParam.getOrderId(), RnrStatus.RELEASED.name());
-        if (!changes.isEmpty()) {
-            result.put(RELEASED_BY, changes.get(0).getCreatedBy().getFirstName() + " " + changes.get(0).getCreatedBy().getLastName());
-            result.put(RELEASED_DATE, dateFormat.format(changes.get(0).getCreatedDate()));
-        }
-        // register the facility related parameters here
-        Facility currentFacility = reportLookupService.getFacilityForRnrId(orderReportParam.getOrderId());
-        result.put(FACILITY_ADDRESS, currentFacility.getAddress1());
-        result.put(PHONE, currentFacility.getMainPhone());
-        result.put(FAX, currentFacility.getFax());
-        result.put(DISTRICT, currentFacility.getGeographicZone().getName());
-        if (currentFacility.getGeographicZone() != null) {
-            result.put(REGION, currentFacility.getGeographicZone().getParent().getName());
-        }
-
-       // ZoneId z = ZoneId.of( "Africa/Nairobi" ) ;
-        //ZonedDateTime zdt = ZonedDateTime.now( z ) ;
+    // ZoneId z = ZoneId.of( "Africa/Nairobi" ) ;
+    //ZonedDateTime zdt = ZonedDateTime.now( z ) ;
 
 /*
         LocalDateTime ldt = LocalDateTime.parse(new Date().toString(), DateTimeFormatter.ofPattern("dd/MM/yy hh:mm a"));
@@ -244,7 +240,7 @@ public class OrderSummaryReportDataProvider extends ReportDataProvider {
         ZonedDateTime tzDateTime = ldt.atZone(ZoneId.of(configurationService.getByKey(Constants.LOCAL_TIME_SETTINGS).toString()));
 */
 
-        result.put("PRINTED_DATE",dateFormat.format(new Date())) ;
-        return result;
-    }
+    result.put("PRINTED_DATE", dateFormat.format(new Date()));
+    return result;
+  }
 }
